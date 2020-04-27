8d94dc81f820982f95db400196221a7bc018423b
sandaka619 <62664778+sandaka619@users.noreply.github.com>
2020-04-28 02:30:06 +0900

    Update task.rb

--- task.rb
+++ task.rb
@@ -34,7 +34,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -44,14 +44,16 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map{|i| i*10 }
+  p numbers2
 end
 
 def q7
