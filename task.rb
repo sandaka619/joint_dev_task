0afbe7c1fbd9e47dc863d616463107c13e934062
sandaka619 <62664778+sandaka619@users.noreply.github.com>
2020-04-28 23:40:51 +0900

    Update task.rb

--- task.rb
+++ task.rb
@@ -60,7 +60,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -69,7 +69,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -79,14 +80,17 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name,i|
+  puts "会員No.#{i} #{name}さん"
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+   puts food.include?("うに") ? "好物です" : "まぁまぁ好きです"
+  end
 end
 
 def q11
